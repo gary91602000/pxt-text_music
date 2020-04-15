41d51a5ec1f6714675fc8704d447d79eba14a7f4
Bing-Hong Chen <51686186+gary91602000@users.noreply.github.com>
2020-04-15 16:04:10 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,16 +8,25 @@
 //% block="test_music" color="#AA278D" 
 //block積木群組名稱,color積木顏色
 namespace test_music {
-
-    // note that Caml casing yields lower case
-    // block text with spaces
-
+    let pin = DigitalPin.P0;
+    
+    //% blockId=setPin 
+    //% block="set BEAT to pin %tbeat"
+    //% tbeat.fieldEditor="gridpicker" 
+    //% tbeat.fieldOptions.columns=4
+    //% tbeat.fieldOptions.tooltips="false" 
+    //% tbeat.fieldOptions.width="300"
+    export function setPin(tbeat: BeatFraction): void {
+        pin = tbeat;
+    }
+    
+    
     //% weight=20
     //% blockId=playtone 
     //% block="playtone | %value=device_note | for | %tbeat=device_beat | beat"
     //% tbeat.shadow="timePicker"
     
-    export function playtone(value: Note, tbeat: BeatFraction): void {
-        music.playTone(value, music.beat(tbeat))
+    export function playtone(value: Note): void {
+        music.playTone(value, music.beat(pin))
     }		
 }
