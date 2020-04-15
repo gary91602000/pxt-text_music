bf041b2dd2d090fbee6fcc8753962ea5a7ad6931
Bing-Hong Chen <51686186+gary91602000@users.noreply.github.com>
2020-04-15 19:34:02 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -24,7 +24,7 @@ namespace test_music {
     //% blockId=ringtone 
     //% block="ringtone | %value=device_note"
     export function ringtone(value: Note): void {
-        music.ringtone(value)
+        music.ringTone(value)
     }
       
 }
